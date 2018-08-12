5e903391bc3e4e8e4da120be30d8351a24f2da50
PCandCG <37952236+PCandCG@users.noreply.github.com>
2018-08-12 11:39:38 +0100

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -5,19 +5,23 @@ int main() {
 	int userChoice;
 	cout << "Please select your favourite beverage from the list" << endl << "1. Lemonade" << endl << "2. Water" << endl << "3. Milk" << endl << "4. Coca Cola" << endl << "5. Appletiser" << endl;
 	cin >> userChoice;
-	if (userChoice == 1) {
+	switch (userChoice) {
+	case 1 :
 		cout << "Lemonade";
-	}
-	else if (userChoice == 2) {
+		break;
+	case 2 :
 		cout << "Water";
-	}
-	else if (userChoice == 3) {
+		break;
+	case 3 :
 		cout << "Milk";
-	}
-	else if (userChoice == 4) {
+		break;
+	case 4 :
 		cout << "Coca Cola";
-	}
-	else if (userChoice == 5) {
+		break;
+	case 5 :
 		cout << "Appletiser";
+		break;
+	default :
+		cout << "Error. choice was not valid, here is your money back";
 	}
 }
