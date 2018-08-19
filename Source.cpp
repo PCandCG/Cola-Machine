c8e54e10d96f42951486c56d78400a3f3cbea3b4
PCandCG <37952236+PCandCG@users.noreply.github.com>
2018-08-19 11:07:55 +0100

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -6,18 +6,19 @@ int main() {
 	cout << "Please select your favourite beverage from the list" << endl << "1. Lemonade" << endl << "2. Water" << endl << "3. Milk" << endl << "4. Coca Cola" << endl << "5. Appletiser" << endl;
 	cin >> userChoice;
 	if (userChoice == 1) {
-		cout << "Lemonade";
+		cout << "Lemonade" << endl;
 	}
 	else if (userChoice == 2) {
-		cout << "Water";
+		cout << "Water" << endl;
 	}
 	else if (userChoice == 3) {
-		cout << "Milk";
+		cout << "Milk" << endl;
 	}
 	else if (userChoice == 4) {
-		cout << "Coca Cola";
+		cout << "Coca Cola" << endl;
 	}
 	else if (userChoice == 5) {
-		cout << "Appletiser";
+		cout << "Appletiser" << endl;
 	}
+	system("PAUSE");
 }
